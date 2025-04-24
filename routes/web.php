b9743859b5c9ec1aa5738aa7b90086ca19b89a78
Rakha <rkhdinar00@gmail.com>
2025-04-25 05:13:45 +0700

    update: profile settings and role auth middleware redirect

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
-Route::get('/order', function () {
-    return view('order');
-})->name('order');
+Route::get("/login", function () {
+    return redirect("/admin/login");
+})->name("login");
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/order', function () {
+        return view('order');
+    })->name('order');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.update-password');
+    Route::post('/profile/logout', [ProfileController::class, 'logout'])->name('profile.logout');
+});
