1246cff79c16d130292f34b9f9e30743c385ce55
Rakha <rkhdinar00@gmail.com>
2025-04-28 23:51:08 +0700

    chore: remove unused middleware and add .env.example

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Middleware\EnsureUserHasRole;
-use App\Http\Middleware\EnsureUserIsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
